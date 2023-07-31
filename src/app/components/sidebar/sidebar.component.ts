0680e5e50cc0cf7e0a4c295f93964197d75df998
Esteban0809 <esteban.ferreiro@hotmail.es>
2023-07-31 14:30:32 +0200

    Agregados estilos y cambios en el componente sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+// src/app/components/sidebar/sidebar.component.ts
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    // Obtener todos los enlaces del menú
+    const navLinks = document.querySelectorAll('.nav-link') as NodeListOf<HTMLAnchorElement>;
+
+    // Convertir la NodeList a un arreglo y usar forEach
+    Array.from(navLinks).forEach((link: HTMLAnchorElement) => {
+      if (link.href && link.href === window.location.href) {
+        link.classList.add('visited');
+      }
+    });
+  }
 
 }
