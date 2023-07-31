b76c70821f939a4b41d42f4388bccb797b5adc3e
Esteban0809 <esteban.ferreiro@hotmail.es>
2023-07-31 12:33:06 +0200

    Generados nuevos componentes: home, blog, sobre-mi, contacto, tienda, sidebar, slider, footer, pagina-error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PaginaErrorComponent } from './components/pagina-error/pagina-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    SobreMiComponent,
+    ContactoComponent,
+    TiendaComponent,
+    SidebarComponent,
+    SliderComponent,
+    FooterComponent,
+    PaginaErrorComponent
   ],
   imports: [
     BrowserModule,
