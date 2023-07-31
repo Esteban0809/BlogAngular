e2b62e9c6ce9578c5ec3a87754dd934a75f6e00d
Esteban0809 <esteban.ferreiro@hotmail.es>
2023-07-31 15:51:32 +0200

    Le dimos un poco de forma al footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { PaginaErrorComponent } from './components/pagina-error/pagina-error.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta para la página de inicio
+  { path: 'blog', component: BlogComponent }, // Ruta para la página del blog
+  { path: 'sobre-mi', component: SobreMiComponent }, // Ruta para la página "Sobre mí"
+  { path: 'contacto', component: ContactoComponent }, // Ruta para la página de contacto
+  { path: 'tienda', component: TiendaComponent }, // Ruta para la página de la tienda
+  { path: '**', component: PaginaErrorComponent }, // Ruta para manejar páginas no encontradas (404)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,3 +1,4 @@
+// src/app/components/footer/footer.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  constructor() { }
 }
